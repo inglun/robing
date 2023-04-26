4d6e735f75297cde89cf1eba66d73dbb484e5ace
Robert Lundberg <robert_e_lundberg@hotmail.com>
2023-04-26 15:07:37 +0200

    .

--- GameOne/Game.cs
+++ GameOne/Game.cs
@@ -11,11 +11,34 @@ namespace GameOne {
 		int shootsFired;
 
 		public void Init() {
-			// Math.Rand
-			map.Add(3, "mål");
-			map.Add(7, "mål");
-			shootsFired = 0;
-		}
+            /*// Math.Rand
+			//abcdef
+			Random rnd = new Random();
+			int rand_num = rnd.Next(0,max);
+			//Console.WriteLine(rand_num);
+            Random rnd2 = new Random();
+            int rand_num2 = rnd2.Next(0,max);
+			//Console.WriteLine(rand_num2);
+			if (rand_num2 == rand_num) {
+				Init();
+			}
+            map.Add(rand_num, "mål");
+			map.Add(rand_num2, "mål");
+			shootsFired = 0;*/
+            Random rnd = new Random();
+
+            int numberTargets = 2;
+            while (numberTargets > 0)
+            {
+                int rand_num = rnd.Next(0, max); // ska det vara max eller max-1
+                if (!map.ContainsKey(rand_num))
+                {
+                    map.Add(rand_num, "mål");
+                    numberTargets--;
+                    Console.WriteLine(rand_num);
+                }
+            }
+        }
 
 		public void Run() {
 			do {
@@ -26,6 +49,7 @@ namespace GameOne {
 				Console.WriteLine("du skrev " + pos);
 				ShootAt(pos);
 			} while(TargetsLeft() > 0);
+			PrintBoard();
 			Console.WriteLine("bye!");
 		}
 		public void ShootAt(int position) {
